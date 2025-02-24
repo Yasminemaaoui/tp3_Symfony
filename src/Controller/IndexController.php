f9ba3c5541e444ea0c8597be498f7b7cb315306b
Yasminemaaoui <yesminemaaoui@gmail.com>
2025-02-24 10:58:10 +0100

    tp3symfony

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -11,6 +11,7 @@ class IndexController extends AbstractController
     #[Route('/{name}', name: 'app_home', defaults: ['name' => 'World'])]
     public function home($name)
     {
-        return $this->render('index.html.twig', ['name' => $name]);
+        $articles = ['Artcile1', 'Article 2','Article 3'];
+        return $this->render('articles/index.html.twig', ['articles' => $articles]);
     }
 }
